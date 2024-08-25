467096ac5d004a4ae4da9b85741c7df8b8e292ac
Thomas Kreuscher <thomas.david.kreuscher@gmail.com>
2024-08-25 21:54:34 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBuaX9NBc5DxQQKaMXIbSHwW1NhN8HJt_c",
+        authDomain: "my-flea-market.firebaseapp.com",
+        projectId: "my-flea-market",
+        storageBucket: "my-flea-market.appspot.com",
+        messagingSenderId: "932852249980",
+        appId: "1:932852249980:web:7a7ff4dd443c32b86b60f9"
+    }
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBuaX9NBc5DxQQKaMXIbSHwW1NhN8HJt_c",
+        authDomain: "my-flea-market.firebaseapp.com",
+        projectId: "my-flea-market",
+        storageBucket: "my-flea-market.appspot.com",
+        messagingSenderId: "932852249980",
+        appId: "1:932852249980:web:7a7ff4dd443c32b86b60f9"
+    }
+  };
+
+
+//   // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBuaX9NBc5DxQQKaMXIbSHwW1NhN8HJt_c",
+//   authDomain: "my-flea-market.firebaseapp.com",
+//   projectId: "my-flea-market",
+//   storageBucket: "my-flea-market.appspot.com",
+//   messagingSenderId: "932852249980",
+//   appId: "1:932852249980:web:7a7ff4dd443c32b86b60f9",
+//   measurementId: "G-MQDHFVKH5S"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
